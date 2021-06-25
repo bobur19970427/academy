566dcf9efd8a3afbd29627197b780828dbaeaa24
Boburjon <bobur.ermonov.97@mail.ru>
2021-06-25 22:37:30 +0500

    birinchi versiya

---
+++ mainapp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+# Create your views here.
+
+from django.http import HttpResponse
+
+class HomePageView(TemplateView):
+    template_name = 'index.html'

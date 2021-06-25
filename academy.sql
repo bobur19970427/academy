b6f7d4b58d80f9aadcbfab69c143f9d1cc7fcae4
Shoxrux Jumanov <shoxrux97jumanov@gmail.com>
2021-06-26 00:17:58 +0500

    baza yangilandi

--- academy.sql
+++ academy.sql
@@ -24,7 +24,10 @@ CREATE TABLE IF NOT EXISTS `auth_group` (
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Экспортируемые данные не выделены.
+-- Дамп данных таблицы academy.auth_group: ~0 rows (приблизительно)
+DELETE FROM `auth_group`;
+/*!40000 ALTER TABLE `auth_group` DISABLE KEYS */;
+/*!40000 ALTER TABLE `auth_group` ENABLE KEYS */;
 
 -- Дамп структуры для таблица academy.auth_group_permissions
 CREATE TABLE IF NOT EXISTS `auth_group_permissions` (
@@ -38,7 +41,10 @@ CREATE TABLE IF NOT EXISTS `auth_group_permissions` (
   CONSTRAINT `auth_group_permissions_group_id_b120cbf9_fk_auth_group_id` FOREIGN KEY (`group_id`) REFERENCES `auth_group` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Экспортируемые данные не выделены.
+-- Дамп данных таблицы academy.auth_group_permissions: ~0 rows (приблизительно)
+DELETE FROM `auth_group_permissions`;
+/*!40000 ALTER TABLE `auth_group_permissions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `auth_group_permissions` ENABLE KEYS */;
 
 -- Дамп структуры для таблица academy.auth_permission
 CREATE TABLE IF NOT EXISTS `auth_permission` (
@@ -51,7 +57,29 @@ CREATE TABLE IF NOT EXISTS `auth_permission` (
   CONSTRAINT `auth_permission_content_type_id_2f476e4b_fk_django_co` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
 
--- Экспортируемые данные не выделены.
+-- Дамп данных таблицы academy.auth_permission: ~18 rows (приблизительно)
+DELETE FROM `auth_permission`;
+/*!40000 ALTER TABLE `auth_permission` DISABLE KEYS */;
+INSERT INTO `auth_permission` (`id`, `name`, `content_type_id`, `codename`) VALUES
+	(1, 'Can add log entry', 1, 'add_logentry'),
+	(2, 'Can change log entry', 1, 'change_logentry'),
+	(3, 'Can delete log entry', 1, 'delete_logentry'),
+	(4, 'Can add permission', 2, 'add_permission'),
+	(5, 'Can change permission', 2, 'change_permission'),
+	(6, 'Can delete permission', 2, 'delete_permission'),
+	(7, 'Can add group', 3, 'add_group'),
+	(8, 'Can change group', 3, 'change_group'),
+	(9, 'Can delete group', 3, 'delete_group'),
+	(10, 'Can add user', 4, 'add_user'),
+	(11, 'Can change user', 4, 'change_user'),
+	(12, 'Can delete user', 4, 'delete_user'),
+	(13, 'Can add content type', 5, 'add_contenttype'),
+	(14, 'Can change content type', 5, 'change_contenttype'),
+	(15, 'Can delete content type', 5, 'delete_contenttype'),
+	(16, 'Can add session', 6, 'add_session'),
+	(17, 'Can change session', 6, 'change_session'),
+	(18, 'Can delete session', 6, 'delete_session');
+/*!40000 ALTER TABLE `auth_permission` ENABLE KEYS */;
 
 -- Дамп структуры для таблица academy.auth_user
 CREATE TABLE IF NOT EXISTS `auth_user` (
@@ -68,9 +96,15 @@ CREATE TABLE IF NOT EXISTS `auth_user` (
   `date_joined` datetime(6) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
--- Экспортируемые данные не выделены.
+-- Дамп данных таблицы academy.auth_user: ~2 rows (приблизительно)
+DELETE FROM `auth_user`;
+/*!40000 ALTER TABLE `auth_user` DISABLE KEYS */;
+INSERT INTO `auth_user` (`id`, `password`, `last_login`, `is_superuser`, `username`, `first_name`, `last_name`, `email`, `is_staff`, `is_active`, `date_joined`) VALUES
+	(1, 'pbkdf2_sha256$100000$Nsm9gflfuQG2$8QGU8ms2cdMYAmPnE5mzgzRX9Ka1kzz/nwJNyGlHpMU=', '2021-06-25 18:37:13.120807', 1, 'webadmin', '', '', 'webadmin@gmail.com', 1, 1, '2021-06-25 18:34:50.058911'),
+	(2, 'vdfvdfvdfv', NULL, 1, 'ngnfnghn', 'gdgdgrg', 'gdrgr', 'fvdfvd@ghfh.hj', 1, 1, '2021-06-26 00:09:55.000000');
+/*!40000 ALTER TABLE `auth_user` ENABLE KEYS */;
 
 -- Дамп структуры для таблица academy.auth_user_groups
 CREATE TABLE IF NOT EXISTS `auth_user_groups` (
@@ -84,7 +118,10 @@ CREATE TABLE IF NOT EXISTS `auth_user_groups` (
   CONSTRAINT `auth_user_groups_user_id_6a12ed8b_fk_auth_user_id` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Экспортируемые данные не выделены.
+-- Дамп данных таблицы academy.auth_user_groups: ~0 rows (приблизительно)
+DELETE FROM `auth_user_groups`;
+/*!40000 ALTER TABLE `auth_user_groups` DISABLE KEYS */;
+/*!40000 ALTER TABLE `auth_user_groups` ENABLE KEYS */;
 
 -- Дамп структуры для таблица academy.auth_user_user_permissions
 CREATE TABLE IF NOT EXISTS `auth_user_user_permissions` (
@@ -98,7 +135,10 @@ CREATE TABLE IF NOT EXISTS `auth_user_user_permissions` (
   CONSTRAINT `auth_user_user_permissions_user_id_a95ead1b_fk_auth_user_id` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Экспортируемые данные не выделены.
+-- Дамп данных таблицы academy.auth_user_user_permissions: ~0 rows (приблизительно)
+DELETE FROM `auth_user_user_permissions`;
+/*!40000 ALTER TABLE `auth_user_user_permissions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `auth_user_user_permissions` ENABLE KEYS */;
 
 -- Дамп структуры для таблица academy.django_admin_log
 CREATE TABLE IF NOT EXISTS `django_admin_log` (
@@ -117,7 +157,10 @@ CREATE TABLE IF NOT EXISTS `django_admin_log` (
   CONSTRAINT `django_admin_log_user_id_c564eba6_fk` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Экспортируемые данные не выделены.
+-- Дамп данных таблицы academy.django_admin_log: ~0 rows (приблизительно)
+DELETE FROM `django_admin_log`;
+/*!40000 ALTER TABLE `django_admin_log` DISABLE KEYS */;
+/*!40000 ALTER TABLE `django_admin_log` ENABLE KEYS */;
 
 -- Дамп структуры для таблица academy.django_content_type
 CREATE TABLE IF NOT EXISTS `django_content_type` (
@@ -128,7 +171,17 @@ CREATE TABLE IF NOT EXISTS `django_content_type` (
   UNIQUE KEY `django_content_type_app_label_model_76bd3d3b_uniq` (`app_label`,`model`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
--- Экспортируемые данные не выделены.
+-- Дамп данных таблицы academy.django_content_type: ~6 rows (приблизительно)
+DELETE FROM `django_content_type`;
+/*!40000 ALTER TABLE `django_content_type` DISABLE KEYS */;
+INSERT INTO `django_content_type` (`id`, `app_label`, `model`) VALUES
+	(1, 'admin', 'logentry'),
+	(3, 'auth', 'group'),
+	(2, 'auth', 'permission'),
+	(4, 'auth', 'user'),
+	(5, 'contenttypes', 'contenttype'),
+	(6, 'sessions', 'session');
+/*!40000 ALTER TABLE `django_content_type` ENABLE KEYS */;
 
 -- Дамп структуры для таблица academy.django_migrations
 CREATE TABLE IF NOT EXISTS `django_migrations` (
@@ -139,7 +192,25 @@ CREATE TABLE IF NOT EXISTS `django_migrations` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
 
--- Экспортируемые данные не выделены.
+-- Дамп данных таблицы academy.django_migrations: ~14 rows (приблизительно)
+DELETE FROM `django_migrations`;
+/*!40000 ALTER TABLE `django_migrations` DISABLE KEYS */;
+INSERT INTO `django_migrations` (`id`, `app`, `name`, `applied`) VALUES
+	(1, 'contenttypes', '0001_initial', '2021-06-25 18:32:30.588587'),
+	(2, 'auth', '0001_initial', '2021-06-25 18:32:42.288072'),
+	(3, 'admin', '0001_initial', '2021-06-25 18:32:44.401110'),
+	(4, 'admin', '0002_logentry_remove_auto_add', '2021-06-25 18:32:44.460536'),
+	(5, 'contenttypes', '0002_remove_content_type_name', '2021-06-25 18:32:46.189238'),
+	(6, 'auth', '0002_alter_permission_name_max_length', '2021-06-25 18:32:46.422523'),
+	(7, 'auth', '0003_alter_user_email_max_length', '2021-06-25 18:32:46.600045'),
+	(8, 'auth', '0004_alter_user_username_opts', '2021-06-25 18:32:46.738926'),
+	(9, 'auth', '0005_alter_user_last_login_null', '2021-06-25 18:32:47.499453'),
+	(10, 'auth', '0006_require_contenttypes_0002', '2021-06-25 18:32:47.542040'),
+	(11, 'auth', '0007_alter_validators_add_error_messages', '2021-06-25 18:32:47.603086'),
+	(12, 'auth', '0008_alter_user_username_max_length', '2021-06-25 18:32:48.838547'),
+	(13, 'auth', '0009_alter_user_last_name_max_length', '2021-06-25 18:32:48.985820'),
+	(14, 'sessions', '0001_initial', '2021-06-25 18:32:49.720540');
+/*!40000 ALTER TABLE `django_migrations` ENABLE KEYS */;
 
 -- Дамп структуры для таблица academy.django_session
 CREATE TABLE IF NOT EXISTS `django_session` (
@@ -150,7 +221,10 @@ CREATE TABLE IF NOT EXISTS `django_session` (
   KEY `django_session_expire_date_a5c62663` (`expire_date`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Экспортируемые данные не выделены.
+-- Дамп данных таблицы academy.django_session: ~0 rows (приблизительно)
+DELETE FROM `django_session`;
+/*!40000 ALTER TABLE `django_session` DISABLE KEYS */;
+/*!40000 ALTER TABLE `django_session` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;

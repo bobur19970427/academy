a948bf8c92987084f5ad869d48d1a6b4555c46a5
Boburjon <bobur.ermonov.97@mail.ru>
2021-06-27 00:53:21 +0500

    register page ishladi

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('about/', AbautPageView.as_view(), name='about'),
     path('news/', NewsPageView, name='news'),
-    path('registration/', RegistrationPageView.as_view(), name='registration'),
+    # path('registration/', RegistrationPageView.as_view(), name='registration'),
     path('<int:post_id>/', News_viewPageView, name='news_view'),
 ]

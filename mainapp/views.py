9f93bc7367f23a55d21feebfaed7f5ad1d43d4eb
Boburjon <bobur.ermonov.97@mail.ru>
2021-06-26 14:37:52 +0500

    news page sozlandi

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from . models import Post
 # Create your views here.
 
 from django.http import HttpResponse
@@ -10,5 +11,11 @@ class HomePageView(TemplateView):
 class AbautPageView(TemplateView):
     template_name = 'about.html'
 
-class NewsPageView(TemplateView):
-    template_name = 'news.html'
+def NewsPageView(request):
+    posts = Post.objects
+    return render(request, 'news.html', {'posts': posts})
+
+
+# class NewsPageView(TemplateView):
+#     posts = Post.objects
+#     template_name = 'news.html'
